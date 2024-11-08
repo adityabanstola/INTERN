b5872d8453087b68f56e70fa9d4f3da260d81c5f
Aditya Banstola <banstola.aditya1@gmail.com>
2024-11-08 13:44:49 +0545

    internwork

---
+++ js/datatypes.js
@@ -0,0 +1,50 @@
+/*
+let x="ankit";
+let y=7;
+
+
+let carName1= "ferrrari"
+let carName2="bugatii"
+
+console.log(x,y,carName1,carName2);
+*/
+
+/*let text = "adityabanstola";
+let length = text.length;
+
+console.log(length);
+
+*/
+
+
+// for functions in js
+
+/*let x= yallahabibi(35,6);
+
+function yallahabibi(x , y){
+    return x + y ;
+}
+console.log(x);
+*/
+
+/*
+
+function toCelsius(f){
+    return (5/9) * (f-32);
+
+}
+
+let value = toCelsius (77);
+console.log(value); 
+*/
+let text = "please visit Hemjaa!";
+let newText = text.replace("Hemjaa","Pokhara");
+
+console.log(newText);
+
+
+let car1= "Maruti";
+let car2="Alto";
+let car3="Centro";
+
+console.log(car1,car2,car3);
---
+++ js/function.js
@@ -0,0 +1,6 @@
+let x=multiply(4,7);
+
+function multiply(a,b){
+       return a * b;
+}
+console.log(x);
